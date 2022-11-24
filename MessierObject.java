2154e245390525ca5b43205bee65833c0dda2823
Anthony de Cruz <anthonydecruz@proton.me>
2022-11-24 17:33:39 +0000

    Created most of MessierObject

--- MessierObject.java
+++ MessierObject.java
@@ -1,20 +1,24 @@
-import java.util.Arrays;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.lang.Math;
 
-public class MessierObject {
+public class MessierObject implements Comparable<MessierObject> {
 
     private String messierNumber;
     private String ngcicNumber;
     private String commonName;
     private String type;
-    private ArrayList<Double> distanceRange;
+    private double distance;
     private String constellation;
     private double apparentMagnitude;
-    private double rightAscension;
-    private double declination;
+    private double rightAscension; // Stored as radians
+    private double declination; // Stored as radians
 
-
+    /**
+     * Constructor with table entry string.
+     * 
+     * @param entry table entry string
+     */
     public MessierObject(String entry) {
 
         String[] values = parseEntry(entry);
@@ -29,30 +33,131 @@ public class MessierObject {
         this.commonName = values[2];
         this.type = values[3];
 
-        this.distanceRange = new ArrayList<Double>();
-
-        try {
-            this.distanceRange.add(Double.parseDouble(values[4].split("-")[0]));
-            this.distanceRange.add(Double.parseDouble(values[4].split("-")[1]));
-
-        } catch (java.lang.NumberFormatException numberFormatException) {
-            System.out.println(numberFormatException);
+        // If distance is a range, take shortest
+        // In future, may have to save as a range and
+        // have a method to return shortest, furthest ect
+        if (values[4].indexOf("-") != -1) {
+            this.distance = Double.parseDouble(values[4].split("-")[0]);
+        } else {
+            this.distance = Double.parseDouble(values[4]);
         }
 
         this.constellation = values[5];
         this.apparentMagnitude = Double.parseDouble(values[6]);
+        this.rightAscension = rightAscensionToRadians(values[7]);
+        this.declination = declinationToRadians(values[8]);
+    }
+
+    /**
+     * Constructor with all values fields.
+     * 
+     * @param messierNumber
+     * @param ngcicNumber
+     * @param commonName
+     * @param type
+     * @param distance
+     * @param constellation
+     * @param apparentMagnitude Stored as radians
+     * @param rightAscension Stored as radians
+     * @param declination
+     */
+    public MessierObject(String messierNumber, String ngcicNumber, String commonName, String type, double distance,
+            String constellation, double apparentMagnitude, double rightAscension, double declination) {
+
+        this.messierNumber = messierNumber;
+        this.ngcicNumber = ngcicNumber;
+        this.commonName = commonName;
+        this.type = type;
+        this.distance = distance;
+        this.constellation = constellation;
+        this.apparentMagnitude = apparentMagnitude;
+        this.rightAscension = rightAscension;
+        this.declination = declination;
 
     }
 
-    private String[] parseEntry(String entry) {
+    /**
+     * Internal method to parse through the given table entry, picking out fields
+     * 
+     * @param entry The table entry line
+     * @return The fields in string array
+     */
+    private static String[] parseEntry(String entry) {
 
-        //todo Make sanitised not remove spaces between words in names
-        String sanitised = entry.replace('"', '\u0000');//.replace('\s', '\u0000');
-        String[] values = sanitised.split(",");
+        String[] values = entry.split(",");
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = values[i].trim();
+        }
+
+        return values;
+
+        // entry = entry.replace('"', '\u0000');
+        // entry = entry.replace('\s', '\u0000');
+    }
+
+    /**
+     * Converts a string with a series of measurements into an ArrayList of doubles.
+     * 
+     * @param measurement The measurement as "(value)(unit) (value)(unit)..."
+     * @return An ArrayList of doubles
+     */
+    private static ArrayList<Double> measurementToDoubles(String measurement) {
+
+        // Convert the string into an arraylist of doubles to be worked with
+        String[] strings = measurement.split(" ");
+        ArrayList<Double> values = new ArrayList<Double>();
+
+        for (String value : strings) {
+            value = value.substring(0, value.length() - 1);
+            values.add(Double.parseDouble(value));
+        }
 
         return values;
     }
-    
+
+    /**
+     * Converts a string of right ascension into radians.
+     * 
+     * @param rightAscension The right ascension as "(hours)h (minutes)m (seconds)s"
+     * @return The right ascension in radians
+     */
+    private static double rightAscensionToRadians(String rightAscension) {
+
+        ArrayList<Double> values = measurementToDoubles(rightAscension);
+
+        return Math.toRadians(values.get(0) + (values.get(1) / 60) + (values.get(2) / 3600));
+    }
+
+    /**
+     * Converts a string of declination into radians.
+     * 
+     * @param declination The given declination as "(degrees)° (arcminutes)'
+     *                    (arcseconds)""
+     * @return The declination in radians
+     */
+    private static double declinationToRadians(String declination) {
+
+        ArrayList<Double> values = measurementToDoubles(declination);
+
+        return Math.toRadians(values.get(0) + (values.get(1) / 60) + (values.get(2) / 3600));
+    }
+
+    @Override
+    /**
+     * Compares apparent magnitude.
+     * 
+     * @param object MessierObject to be compared
+     * @return Comparison integer
+     */
+    public int compareTo(MessierObject object) {
+
+        return Double.compare(this.getApparentMagnitude(), object.getApparentMagnitude());
+    }
+
+    /**
+     * @return A string containing all fields in the original format
+     */
     public String toString() {
 
         Field[] fields = this.getClass().getDeclaredFields();
@@ -62,10 +167,11 @@ public class MessierObject {
             try {
                 if (properties != "") {
                     properties += ", " + field.get(this);
+
                 } else {
                     properties += field.get(this);
                 }
-                
+
             } catch (IllegalAccessException exception) {
                 exception.printStackTrace();
             }
@@ -73,9 +179,8 @@ public class MessierObject {
 
         return properties;
     }
-    
-    /* --------------------------- Getters and Setters -------------------------- */
 
+    /* --------------------------- Getters and Setters -------------------------- */
 
     public String getMessierNumber() {
         return this.messierNumber;
@@ -83,5 +188,33 @@ public class MessierObject {
 
     public String getNgcicNumber() {
         return this.ngcicNumber;
+    }
+
+    public String getCommonName() {
+        return this.commonName;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public double getDistance() {
+        return this.distance;
+    }
+
+    public String getConstellation() {
+        return this.constellation;
+    }
+
+    public double getApparentMagnitude() {
+        return this.apparentMagnitude;
+    }
+
+    public double getRightAscension() {
+        return this.rightAscension;
+    }
+
+    public double getDeclination() {
+        return this.declination;
     }
 }
--- MessierProgram.java
+++ MessierProgram.java
@@ -2,12 +2,22 @@ public class MessierProgram {
 
     public static void main(String[] args) {
 
-        String demo = "M1, \"NGC 1952\", \"Crab Nebula\", Supernova remnant, 4.9-8.1, Taurus, 8.4, 5h 34m 31.9400s, 22° 0' 52.2000";
+        String demo1 = "M1, \"NGC 1952\", \"Crab Nebula\", Supernova remnant, 4.9-8.1, Taurus, 8.4, 5h 34m 31.9400s, 22° 0' 52.2000\"";
+        String demo2 = "M2, \"NGC 7089\", \"-\", Globular cluster, 33.0, Aquarius, 6.5, 21h 33m 27.0200s, 0° 49' 23.7000\"";
 
-        MessierObject bob = new MessierObject(demo);
+        MessierObject bob = new MessierObject(demo1);
+        MessierObject jim = new MessierObject(demo2);
 
-        System.out.println(demo);
+        System.out.println(demo1);
         System.out.println(bob.toString());
         System.out.println(bob.getNgcicNumber());
+
+        System.out.println(demo2);
+        System.out.println(jim.toString());
+        System.out.println(jim.getNgcicNumber());
+
+        System.out.println(jim.compareTo(bob));
+
+        System.out.println(jim.getRightAscension());
     }
 }
