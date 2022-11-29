78ba6647c2c574a0a03e134134b255da61e2b20b
Anthony de Cruz <anthonydecruz@proton.me>
2022-11-29 18:53:38 +0000

    Added doc comment

--- src/MessierProgram/InvalidEntryException.java
+++ src/MessierProgram/InvalidEntryException.java
@@ -1,5 +1,8 @@
 package src.MessierProgram;
 
+/**
+ * Thrown when an argument is in an invalid format.
+ */
 public class InvalidEntryException extends Exception {
     public InvalidEntryException(String message) {
         super(message);
