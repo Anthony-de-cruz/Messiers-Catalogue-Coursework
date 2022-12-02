75d68b6f3a78421cc9a8622701e82ff9b11e5f80
Anthony de Cruz <anthonydecruz@proton.me>
2022-12-02 17:17:26 +0000

    Updated tests

--- src/tests/TestHarness.java
+++ src/tests/TestHarness.java
@@ -10,78 +10,40 @@ public class TestHarness {
     // Should use a test framework but thats out of the scope of the project and
     // target environment.
 
-    public static boolean test1() {
-
-        String demo1 = "M1, \"NGC 1952\", \"Crab Nebula\", Supernova remnant, 4.9-8.1, Taurus, 8.4, 5h 34m 31.9400s, 22° 0' 52.2000\"";
-        String demo2 = "M2, \"NGC 7089\", \"-\", Globular cluster, 33.0, Aquarius, 6.5, 21h 33m 27.0200s, 0° 49' 23.7000\"";
-        String demo3 = "M17, \"NGC 6618\", \"Omega, Swan, Horseshoe, Lobster, or Checkmark Nebula\", H II region nebula with cluster, 5.0-6.0, Sagittarius, 6.0, 18h 20m 26.0000s, -16° 10' 36.0000\"";
-        String demo4 = "M94, \"NGC 4736\", \"Crocodile Eye or Cat's Eye Galaxy\", Spiral galaxy, 14700.0-17300.0, Canes Venatici, 8.2, 12h 50m 53.1000s, 41° 7' 14.0000\"";
-
-        try {
-            System.out.println("\n--- Obj1 ---");
-
-            MessierObject obj1 = new MessierObject(demo3);
-            System.out.println(demo3);
-            System.out.println(obj1.toString());
-            System.out.println(obj1.getNgcicNumber());
-            System.out.println(obj1.getDeclinationAngle());
-
-            System.out.println("\n--- Obj2 ---");
-
-            MessierObject obj2 = new MessierObject(demo4);
-            System.out.println(demo4);
-            System.out.println(obj2.toString());
-            System.out.println(obj2.getMeanDistance());
-            System.out.println(obj2.getRightAscensionTime());
-
-            System.out.println("\n--- Comparison ---");
-
-            System.out.println(obj1.compareTo(obj2));
-
-            System.out.println("\n--- Catalogue ---");
-
-            MessierCatalogue catalogue = new MessierCatalogue(new MessierObject[] { obj1, obj2 });
-
-            System.out.println(catalogue.toString());
-
-        } catch (InvalidEntryException exception) {
-            exception.printStackTrace();
-
-            return false;
-        }
-
-        return true;
-    }
-
     /**
-     * Testing MessierObject
+     * Testing MessierObject.
      * 
      * @return Test result
      */
-    public static boolean test2() {
+    public static boolean testMessierObject() {
 
-        String[] validData = {
+        String[] validStringData = {
                 "M1, \"NGC 1952\", \"Crab Nebula\", Supernova remnant, 4.9-8.1, Taurus, 8.4, 5h 34m 31.9400s, 22° 0' 52.2000\"",
                 "M2, \"NGC 7089\", \"-\", Globular cluster, 33.0, Aquarius, 6.5, 21h 33m 27.0200s, 0° 49' 23.7000\"",
                 "M17, \"NGC 6618\", \"Omega, Swan, Horseshoe, Lobster, or Checkmark Nebula\", H II region nebula with cluster, 5.0-6.0, Sagittarius, 6.0, 18h 20m 26.0000s, -16° 10' 36.0000\"",
                 "M94, \"NGC 4736\", \"Crocodile Eye or Cat's Eye Galaxy\", Spiral galaxy, 14700.0-17300.0, Canes Venatici, 8.2, 12h 50m 53.1000s, 41° 7' 14.0000\"",
         };
 
-        String[] invalidData = {
+        String[] invalidStringData = {
                 "M1, \"NGC 1952\", \"Crab Nebula\", Supernova remnant, 4.9-8.1, Taurus, 8.4, 5h 34m 31.9400s, 22° 0' 52.2000\", hello",
-                "M1, \"NGC 1952\", \"Crab \"Nebula\", Supernova r,emnant, 4.9-8.1, 8.4, 5h 34m 31.9400s, 22° 0' 52.2000\"",
+                "M1, \"NGC 1952\", \"Crab \"Nebula\", Supernova r,emnant, 4.9-8.1-5.2, 8.4, 5h 34m 31.9400s, 22° 0' 52.2000\"",
                 "M1, \"NGC 1952\", \"Crab Nebula\", Supernova remnant, 4.9-8.1, Taurus, 8.4, 5h 34m 31.9400s, 22° 0' 52.2000\"",
                 "M2a, \"NGC 7089\", \"-\", Globular cluster, 33.0, Aquarius, 6.5, 21h 33m 27.0200s, 0° 49' 23.7000\"",
+                "M2a, \"NGC 7089\", \"-\", Globular cluster, 33.0, Aquarius, 6.5, -21h 33m 27.0200s, 0° 49' 23.7000\"",
+                "M94, \"NGC 4736\", \"Crocodile Eye or Cat's Eye Galaxy or \", Spiral galaxy, 14700.0-17300.0, Canes Venatici, 8.2, 12h 50m 53.1000s, 41° 7' 14.0000\"",
         };
 
+        /* ---------------------------- Test Constructor ---------------------------- */
+        /* ------------------------ and in turn, the setters ------------------------ */
+
         // Valid test data
-        for (int i = 0; i < validData.length; i++) {
+        for (int i = 0; i < validStringData.length; i++) {
 
             try {
-                MessierObject obj = new MessierObject(validData[i]);
+                MessierObject obj = new MessierObject(validStringData[i]);
 
-                if (!obj.toString().equals(validData[i])) {
-                    System.err.println(validData[i]);
+                if (!obj.toString().equals(validStringData[i])) {
+                    System.err.println(validStringData[i]);
                     System.err.println(obj.toString());
                     System.err.println("Test 2, valid case " + i + ": FAILED");
                     return false;
@@ -89,41 +51,45 @@ public class TestHarness {
 
             } catch (InvalidEntryException exception) {
                 exception.printStackTrace();
-                System.err.println(validData[i]);
+                System.err.println(validStringData[i]);
                 System.err.println("Test 2, valid case " + i + ": FAILED");
                 return false;
             }
 
-            System.out.println("Test 2, valid case " + i + ": PASSED");
+            System.out.println("Valid case " + i + ": PASSED");
         }
 
         // Erroneous test data
-        for (int i = 0; i < invalidData.length; i++) {
+        for (int i = 0; i < invalidStringData.length; i++) {
 
             try {
-                MessierObject obj = new MessierObject(invalidData[1]);
+                MessierObject obj = new MessierObject(invalidStringData[1]);
 
-                System.err.println(validData[i]);
+                System.err.println(validStringData[i]);
                 System.err.println(obj.toString());
-                System.err.println("Test 2, valid case " + i + ": FAILED");
+                System.err.println("Valid case " + i + ": FAILED");
                 return false;
 
             } catch (InvalidEntryException exception) {
 
-                System.out.println("Test 2, invalid case " + i + ": PASSED");
+                System.out.println("Invalid case " + i + ": PASSED");
             }
         }
 
-        System.out.println("\n -- Test 2: PASSED -- \n");
+        /* ---------------------- Test the rest of the Methods ---------------------- */
+
+
+
+        System.out.println("\n -- testMessierObject: PASSED -- ");
         return true;
     }
 
     /**
-     * Testing MessierCatalogue
+     * Testing MessierCatalogue.
      * 
      * @return Test result
      */
-    public static boolean test3() {
+    public static boolean testMessierCatalogue() {
 
         String[] validData = {
                 "M1, \"NGC 1952\", \"Crab Nebula\", Supernova remnant, 4.9-8.1, Taurus, 8.4, 5h 34m 31.9400s, 22° 0' 52.2000\"",
@@ -162,7 +128,18 @@ public class TestHarness {
             }
         }
 
-        System.out.println("\n -- Test 3: PASSED -- \n");
+        System.out.println("\n -- testMessierCatalogue: PASSED -- ");
+        return true;
+    }
+
+    /**
+     * Testing IOHandler.
+     * 
+     * @return Test result
+     */
+    public static boolean testIOHandler() {
+
+        System.out.println("\n -- testIOHandler: PASSED -- ");
         return true;
     }
 
@@ -176,8 +153,9 @@ public class TestHarness {
         boolean passed = true;
 
         // passed = passed && test1();
-        passed = passed && test2();
-        passed = passed && test3();
+        passed = passed && testMessierObject();
+        passed = passed && testMessierCatalogue();
+        passed = passed && testIOHandler();
 
         return passed;
     }
